217408c70b8450b3eb9e941baeb642379cd7cdd5
Alexey Zakharov <embalex@Alexeys-MacBook-Pro.local>
2018-08-07 12:13:31 +0300

    Storybook is deleted

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  require('../src/stories/menu.stories.js');
-  // You can require as many stories as you need.
-}
-
-configure(loadStories, module);
